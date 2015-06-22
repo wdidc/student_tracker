6de4ef4d9583fdc45b60d03bef96fdda6881dc53
Andy <andrew.sunglae.kim@gmail.com>
2015-06-22 15:59:35 -0400

    add strong params to statuses controller

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -6,4 +6,19 @@ class StatusesController < ApplicationController
     @student = HTTParty.get("http://api.wdidc.org/students/#{params[:github_id]}").body
   end
 
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+  private
+  def status_params
+    params.require(:status).permit(:color, :body, :github_id)
+  end
 end
