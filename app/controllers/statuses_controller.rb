d73f77a309cc03aa39accf57b525d8ef66ff8210
Andy <andrew.sunglae.kim@gmail.com>
2015-06-22 16:11:38 -0400

    add query on show

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -3,13 +3,14 @@ class StatusesController < ApplicationController
       @students = HTTParty.get("http://api.wdidc.org/students")
   end
   def show
+    @statuses = Status.where(github_id: params[:github_id])
     @status = Status.new
     @student = JSON.parse(HTTParty.get("http://api.wdidc.org/students/#{params[:github_id]}").body)
   end
   def create
     @status = Status.new(status_params)
     if @status.save
-      redirect_to @student
+      redirect_to "/#{@status.github_id}"
     end
   end
 
@@ -18,7 +19,7 @@ class StatusesController < ApplicationController
     # need to be able to query for status you're updating
     @status.update(status_params)
     if @status.save
-      redirect_to @student
+      redirect_to "/#{@status.github_id}"
     end
   end
 
