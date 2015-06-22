c44081e6508b7249ee4e2cd6a37ac1e650c70541
Jesse Shawl <jesse@jshawl.com>
2015-06-22 15:56:38 -0400

    add show page

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,7 +1,9 @@
 class StatusesController < ApplicationController
   def index
       @students = HTTParty.get("http://api.wdidc.org/students")
-      binding.pry
+  end
+  def show
+    @student = HTTParty.get("http://api.wdidc.org/students/#{params[:github_id]}").body
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Status.create(color: "red", github_id: 1, body: "This person is doing not great" )
+Status.create(color: "red", github_id: 1, body: "Still working on being better" )
+Status.create(color: "green", github_id: 2, body: "Amazing student" )
