795fbefab960168f58b3081bf3b4a7f2b525cde1
Andy <andrew.sunglae.kim@gmail.com>
2015-07-14 19:42:53 -0400

    student colors

--- app/models/student.rb
+++ app/models/student.rb
@@ -27,11 +27,52 @@ class Student
 		student = students.find{|student|student["github_user_id"] == id.to_i}
 	end
 
+  def hue_to_rgb(p,q,t)
+    if t < 0
+      t += 1
+    end
+    if t > 1
+      t -= 1
+    end
+    if t < 1/6.0
+      return (p + (q-p) * 6 * t)
+    end
+    if t < 1/2.0
+      return q
+    end
+    if t < 2/3.0
+      return (p + (q - p) * (2/3.0 - t) * 6)
+    end
+      return p
+  end
+
+  def hsl_to_rgb(h, s, l)
+    if s == 0
+      b = l.to_f
+      g = l.to_f
+      r = l.to_f
+    else
+      q = l.to_f < 0.5 ? (l.to_f * (1 + s.to_f)) : (l.to_f + s.to_f - l.to_f * s.to_f)
+      p = 2 * l.to_f - q;
+      r = hue_to_rgb(p, q, (h + 1/3.0))
+      g = hue_to_rgb(p, q, h)
+      b = hue_to_rgb(p, q, (h - 1/3.0))
+    end
+    return [(r * 255).to_i, (g * 255).to_i,(b * 255).to_i ]
+  end
+
 	def color
-	  status = Status.where(github_id: self.github_id).last
-	  if status
-	    return status.color
-	  end
+	  statuses = Status.where(github_id: self.github_id)
+    red = statuses.where(color: "red").length
+    yellow = statuses.where(color: "yellow").length
+    green = statuses.where(color: "green").length
+    red += yellow/2.0
+    green += yellow/2.0
+    total = green + red
+    green_percent = green/total.to_f
+    hue = green_percent * 1.2 / 360 * 100
+    rgb = hsl_to_rgb(hue, 1, 0.5)
+    return "rgb(#{rgb[0]}, #{rgb[1]}, #{rgb[2]})"
 	end
 
   def github_repo_url
@@ -44,6 +85,4 @@ class Student
 	    return status.updated_at
 	  end
 	end
-
-
 end
