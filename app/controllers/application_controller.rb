da14dc3a1d6617dd4bd2043701f1558bc4f9eae0
Jesse Shawl <jesse@jshawl.com>
2015-07-31 14:12:54 -0400

    reuse helper code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user, except: [:authenticate]
   protect_from_forgery with: :exception
-  def current_user
-    User.find_by(uid: session[:uid])
-  end
+  include StatusesHelper
   private
   def authenticate_user
     unless current_user
