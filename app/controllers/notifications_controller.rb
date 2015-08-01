3a9c62ce2b7f4d0d55b7940f47c789e60af504f7
Jesse Shawl <jesse@jshawl.com>
2015-08-01 10:54:09 -0400

    add destroy

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -9,6 +9,11 @@ class NotificationsController < ApplicationController
       format.json { render json: @notifications_count }
     end
   end
+  def destroy
+    @notification = Notification.find(params[:id])
+    @notification.destroy
+    redirect_to notifications_path
+  end
   private
   def notification_params
     params.require(:notification).permit(:read)
